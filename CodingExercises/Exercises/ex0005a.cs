0fa0e84aa67c3d6880100fdb1c99ec4965739fa5
Jerry Howard <jerryhoward@fastmail.fm>
2025-06-23 18:30:10 -0600

    A refined version of Ex0005 (Ex0005a). Works the same. Wanted to play with a slightly leaner loop over the string array.

---
+++ CodingExercises/Exercises/ex0005a.cs
@@ -0,0 +1,63 @@
+using Exercises;
+
+/*
+Topic: Arrays / Hash Tables / Strings
+Difficulty: Medium
+Problem: Group Anagrams
+
+Description:
+Write a method that takes an array of strings strs and groups the anagrams together. 
+You can return the answer in any order.
+
+Examples:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Input: strs = [""]
+Output: [[""]]
+
+Input: strs = ["a"]
+Output: [["a"]]
+*/
+
+[Exercise("ex0005a")]
+public class Ex0005a : IExercise
+{
+    public void Run()
+    {
+        string[] input = ["eat","tea","tan","ate","nat","bat"];
+        IList<IList<string>> output = GroupAnagrams(input);
+        Console.WriteLine(string.Join("\n", output.Select(row => string.Join(", ", row))));
+    }
+
+    public IList<IList<string>> GroupAnagrams(string[] strs) 
+    {
+        // We declare that our concrete list will hold items of the INTERFACE type.
+        var anagramGroups = new List<IList<string>>();
+
+        Dictionary<string, List<string>> d = new Dictionary<string, List<string>>();
+
+        for (int x = 0; x < strs.Length; x++)
+        {
+            // sort the string
+            var sorted = strs[x].ToArray();
+            Array.Sort(sorted);
+            string sortedStr = string.Join("", sorted);
+
+            if (!d.ContainsKey(sortedStr))
+            {
+                d.Add(sortedStr, new List<string>());
+            }
+            
+            d[sortedStr].Add(strs[x]);
+        }
+
+        foreach(KeyValuePair<string, List<string>> pair in d)
+        {
+            anagramGroups.Add(pair.Value);
+        }
+
+        return anagramGroups;
+    }
+}
